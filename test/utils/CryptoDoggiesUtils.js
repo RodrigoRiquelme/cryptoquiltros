2178d3fc8962b92ffcd3caadd85005f478c8f791
Rodrigo Riquelme <rodrigo.riquelme.e@gmail.com>
2018-07-22 16:14:41 -0400

    fixes

--- test/utils/CryptoDoggiesUtils.js
+++ test/utils/CryptoDoggiesUtils.js
@@ -1,19 +1,26 @@
-module.gexports = function (CryptoDoggies, accounts) {
-    function checkDoggyCreation(age, name, dna) {
-        it('createDoggy should be a doggy named ' + name, function(done){
-            CryptoDoggies.deployed().then(async function(instance){
-                await instance.createDoggy(age, name, dna, {
-                    from: accounts[0]
-                }).then(
-                    function (result) {
-                        assert.include(result.logs[0].event, 'DoggyCreated', 'DoggyCreatedEvent was not triggered');
-                    }
-                );
+module.exports = function (CryptoDoggies, accounts) {
+    function checkTotalSupply (expectedValue) {
+        it('totalSupply should be equal to ' + expectedValue, function(done){
+            CryptoDoggies.deployed().then(function(instance){
+                instance.totalSupply.call().then(function(totalSupply){
+                    assert.equal(totalSupply, expectedValue, 'totalSupply is not equal to ' + expectedValue);
+                }).then(done).catch(done);
+            });
+        });
+    }
+
+    function checkDoggyCreation (name) {
+        it('createToken should create a random doggy named ' + name, function(done) {
+            CryptoDoggies.deployed().then(async function(instance) {
+                await instance.createToken(name, { from: accounts[0] }).then(function(result){
+                    assert.include(result.logs[0].event, 'TokenCreated', 'TokenCreated event was not triggered');
+                });
             }).then(done).catch(done);
         });
-    };
+    }
 
     return {
-        checkDoggyCreation: checkDoggyCreation
+        checkTotalSupply: checkTotalSupply,
+        checkDoggyCreation: checkDoggyCreation,
     }
 }
